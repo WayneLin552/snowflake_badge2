8c684596143f6a63c48208ee6dc2fcf0efab2a81
Mouwei Lin <lm3756@columbia.edu>
2023-05-06 09:22:02 -0400

    Update streamlit_app.py
    emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
-streamlit.title('Hello World')
+streamlit.title("My Parents' New Healthy Dinner")
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
